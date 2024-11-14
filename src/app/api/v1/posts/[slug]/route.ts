bacb86a6d3d91265d7b4a054476a3145e0e198bb
Giovanni <giovanni@192.168.0.101>
2024-11-14 12:02:15 -0500

    Add edit age data flow

--- src/app/api/v1/posts/[slug]/route.ts
+++ src/app/api/v1/posts/[slug]/route.ts
@@ -1,13 +1,11 @@
 import { NextResponse } from 'next/server';
 import { getPostData } from '../../../../../model/posts.model';
-import { error } from 'console';
 
 const GET = async (_request, { params }) => {
     const { slug } = await params;
     const post = await getPostData({ slug });
-    console.log(post);
-
-    if (!post) return NextResponse.json({ message: 'Post not found' }, { status: 404 });
+    
+    if (!post) return NextResponse.json({ message: `Post with slug ${slug} not found` }, { status: 404 });
 
     return NextResponse.json(post);
 };
--- src/app/edit/[slug]/page.tsx
+++ src/app/edit/[slug]/page.tsx
@@ -1,9 +1,67 @@
-import { getPostData } from "../../../model/posts.model";
+"use client";
 
-const EditPage = async ({ params }) => {
-    const { slug } = await params;
-    const post = await getPostData({ slug });
-    return <h1>Edit content for {`${slug}`}</h1>;
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import { use } from "react";
+
+type typeParams = { slug: string };
+
+const EditPage = ({ params }) => {
+    const [post, setPost] = useState(null);
+    const [mounted, setMounted] = useState(false);
+
+    const unwrappedParams = use(params);
+    const { slug } = (unwrappedParams as typeParams);
+    
+    // const router = useRouter();
+
+    useEffect(() => {
+        setMounted(true);
+    }, []);
+
+    useEffect(() => {
+        if (!mounted) return;
+
+        const fetchPostData = async () => {
+        const response = await fetch(`/api/v1/posts/${slug}`);
+        const postData = await response.json();
+        setPost(postData);
+        };
+
+        fetchPostData();
+    }, [slug, mounted]);
+
+    if (!mounted) return <p>Loading...</p>;
+
+    if (!post) return <p>Loading post...</p>;
+
+    return (
+        <>
+        <h2>Update Article</h2>
+        <form onSubmit={(e) => e.preventDefault()}>
+            <label>Article title</label>
+            <input
+            type="text"
+            id="title"
+            value={post.title}
+            onChange={(e) => setPost({ ...post, title: e.target.value })}
+            />
+            <label>Publishing Date</label>
+            <input
+            type="date"
+            id="date"
+            value={post.date}
+            onChange={(e) => setPost({ ...post, date: e.target.value })}
+            />
+            <label>Content</label>
+            <textarea
+            value={post.content}
+            onChange={(e) => setPost({ ...post, content: e.target.value })}
+            />
+            <input type="button" value="Update" />
+        </form>
+        </>
+    );
 };
 
 export default EditPage;
